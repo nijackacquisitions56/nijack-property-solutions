50e87600963a43b719cae3fdf45e2a75f8eee613
nijackacquisitions56 <nijack.acquisitions@gmail.com>
2026-03-30 01:40:54 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -426,12 +426,14 @@ const NijackSite = () => {
                 inquiry. Depending on the situation, we may work with trusted
                 investment partners to help create the best possible solution for
                 you.
-                We are real estate investors and do not represent buyers or sellers as 
+              </p>
+              </p>
+                 We are real estate investors and do not represent buyers or sellers as 
                 licensed agents. We may enter into purchase agreements and assign those 
                 agreements to other buyers. 
               </p>
               <p>
-              We aim to keep the process simple, transparent, and respectful so
+                We aim to keep the process simple, transparent, and respectful so
                 you can decide what works best for your situation.
               </p>
             </div>
