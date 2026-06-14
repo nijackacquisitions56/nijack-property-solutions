43dcaf000aac25211eeeecf39be24def39493fb1
nijackacquisitions56 <nijack.acquisitions@gmail.com>
2026-06-14 06:01:58 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 const App = () => {
+  useEffect(() => { window.scrollTo(0, 0); }, []);
   const [openFaq, setOpenFaq] = useState(null);
   const [step, setStep] = useState(1);
   const [formData, setFormData] = useState({
