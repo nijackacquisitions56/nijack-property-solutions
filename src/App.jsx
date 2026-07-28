3ffad4fea976ece6252f975a61ff43a6748f8969
nijackacquisitions56 <nijack.acquisitions@gmail.com>
2026-07-28 03:00:23 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -182,7 +182,7 @@ const App = () => {
   style={{
     background: '#8B0000',
     padding: '10px 20px 70px',
-    marginTop: '-85px',
+    marginTop: '-170px',
     position: 'relative',
     zIndex: 3
   }}
