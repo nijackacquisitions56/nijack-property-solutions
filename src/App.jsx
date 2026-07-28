3bd14844f682b806c57fbe0afd7a5d476bc3d77d
nijackacquisitions56 <nijack.acquisitions@gmail.com>
2026-07-28 00:44:17 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -522,7 +522,7 @@ const App = () => {
                           return;
                         }
                         if (!formData.timeline) { setErrors({ step2: 'Please select your ideal selling time before submitting.' }); return; }
-                        if (!formData.priceExpectation || formData.priceExpectation.trim() === '') { setErrors({ step2: 'Please enter a price you would consider for a cash offer.' }); return; }
+                        if (!formData.priceExpectation || formData.priceExpectation.trim() === '') { setErrors({ step2: 'Please enter a price you would be comfortable accepting.' }); return; }
                         if (!formData.additionalNotes || formData.additionalNotes.trim() === '') { setErrors({ step2: 'Please give us additonal notes about the property before submitting.' }); return; }
                         if (!formData.preferredContact) { setErrors({ step2: 'Please select your preferred contact method before submitting.' }); return; }
                         if (
